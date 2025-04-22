6c74cf93d5ec4da7bb3d844cab8513351f197c76
osama Elgendy <osamaElgendy12345677889@gmail.com>
2025-04-22 17:53:32 +0200

    add enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from the Next.js frontend
+    credentials: true, // Allow cookies and credentials
+  });
+
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe(
-    {
-     whitelist:true,
-     forbidNonWhitelisted:true,
-     transform:true,
-    }
-  ));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
