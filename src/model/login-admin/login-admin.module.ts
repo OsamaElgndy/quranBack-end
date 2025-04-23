b9eaddc36d2351579f03b65209bf747ed63e0ca4
osama Elgendy <osamaElgendy12345677889@gmail.com>
2025-04-23 14:48:57 +0200

    fix Add SendEmailService to providers

--- src/model/login-admin/login-admin.module.ts
+++ src/model/login-admin/login-admin.module.ts
@@ -1,17 +1,13 @@
+// src/model/login-admin/login-admin.module.ts
 import { Module } from '@nestjs/common';
 import { LoginAdminService } from './login-admin.service';
 import { LoginAdminController } from './login-admin.controller';
-import { envVariables } from 'src/common/constants/variables.service';
 import { JwtModule } from '@nestjs/jwt';
-import { SendEmailService } from 'src/common/sendEmail/mail.service';
-console.log(envVariables)
+import { SendEmailService } from 'src/common/sendEmail/mail.service'; // Import SendEmailService
+
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: envVariables.JWT_SECRET,
-    }),
-  ],
+  providers: [LoginAdminService, SendEmailService], // Add SendEmailService to providers
   controllers: [LoginAdminController],
-  providers: [LoginAdminService,SendEmailService],
+  imports: [JwtModule],
 })
 export class LoginAdminModule {}
