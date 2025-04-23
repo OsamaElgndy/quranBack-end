0a6ba4d7cbfa3a5ac6b4bb47808f19c5d6488fca
John Doe <johndoe@example.com>
2025-04-23 04:54:39 +0200

    add res student

--- src/model/login-student/login-student.service.ts
+++ src/model/login-student/login-student.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-login-student.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { HttpException } from '@nestjs/common';
 @Injectable()
 export class LoginStudentService {
   constructor(private readonly prisma: PrismaService) {}
@@ -15,6 +15,36 @@ export class LoginStudentService {
 
   async findAll() {
     return this.prisma.student.findMany();
+  } 
+  async findOne(id: number) {
+   
+    // Check if the student exists
+    const student = await this.prisma.student.findUnique({
+      where: { id },
+    });
+
+    if (!student) {
+      throw new HttpException('لم يتم العثور على الطالب', 404);
+    }
+    return this.prisma.student.findUnique({
+      where: { id },
+    });
   }
+
+  async update(id: number, updateStudentDto: CreateStudentDto) {
+    // Check if the student exists
+    const student = await this.prisma.student.findUnique({
+      where: { id },
+    });
+
+    if (!student) {
+      throw new HttpException('لم يتم العثور على الطالب', 404);
+    }
+    return this.prisma.student.update({
+      where: { id },
+      data: updateStudentDto,
+    });
+  }
+  
 
 }
