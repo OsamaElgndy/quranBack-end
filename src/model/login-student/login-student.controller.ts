777f298da0658f7f0dd2fedada31cebc23599298
osama Elgendy <osamaElgendy12345677889@gmail.com>
2025-04-26 15:40:15 +0300

    add type on  isActive

--- src/model/login-student/login-student.controller.ts
+++ src/model/login-student/login-student.controller.ts
@@ -12,9 +12,9 @@ export class LoginStudentController {
   @Get("isActive")
   findAllisActive(
 
-    @Query('skip', ParseIntPipe) skip: number = 0,
-    @Query('take', ParseIntPipe) take: number = 10
-    
+    @Query('skip') skip: number = 0,
+    @Query('take') take: number = 10
+
   ) {
     return this.loginStudentService.findAllisActive(skip, take);
   }
@@ -22,8 +22,8 @@ export class LoginStudentController {
 
   @Get("isNotActive")
   findAllisNotActive(
-    @Query('skip', ParseIntPipe) skip: number = 0,
-    @Query('take', ParseIntPipe) take: number = 10
+    @Query('skip') skip: number = 0,
+    @Query('take') take: number = 10
   )
     {
     return this.loginStudentService.findAllisNotActive(skip , take );
